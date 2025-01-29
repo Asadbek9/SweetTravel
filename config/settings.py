904b72cfd1fc6d5b59c0bb72d78f3442cf1efa86
asadbek <asadrikhsibayev27@gmail.com>
2025-01-29 16:30:46 +0500

    media-files1

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static-files/'
+STATIC_ROOT = os.path.join('static-files/')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join("static/")
 ]
 
 MEDIA_URL = 'media/'
